d4e96cc3e0f89a2647534b965547ad162a1c2da8
MuizZatam <muizzatam110@gmail.com>
2025-03-23 09:19:25 +0000

    Create client and set-up keys

---
+++ src/main.py
@@ -0,0 +1,23 @@
+import tweepy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_KEY = os.environ.get('API_KEY')
+API_KEY_SECRET = os.environ.get('API_KEY_SECRET')
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
+
+
+def main():
+
+    client = tweepy.Client(
+
+        consumer_key=API_KEY,
+        consumer_secret=API_KEY_SECRET,
+        access_token=ACCESS_TOKEN,
+        access_token_secret=ACCESS_TOKEN_SECRET
+    )
+
+    test_tweet = "Return the Slab!!!"
+    client.create_tweet(text = test_tweet)
